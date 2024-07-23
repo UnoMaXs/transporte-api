5ffcc2ac542bed38cbaddc9c1805f18a0a184cdd
Aiken Rojas <pieraiken202@hotmail.com>
2024-07-22 22:01:58 -0500

    code smells

--- src/main/java/com/example/backendtransporteapi/model/DailySummary.java
+++ src/main/java/com/example/backendtransporteapi/model/DailySummary.java
@@ -3,7 +3,6 @@ package com.example.backendtransporteapi.model;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDate;
 
 @Getter
--- src/main/java/com/example/backendtransporteapi/repository/TransactionRepository.java
+++ src/main/java/com/example/backendtransporteapi/repository/TransactionRepository.java
@@ -2,10 +2,10 @@ package com.example.backendtransporteapi.repository;
 
 import com.example.backendtransporteapi.model.TransactionModel;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.time.LocalDateTime;
 import reactor.core.publisher.Flux;
 
-public interface TransactionRepository extends ReactiveCrudRepository<TransactionModel, Long> {
+import java.time.LocalDateTime;
+
+public interface TransactionRepository extends ReactiveCrudRepository<TransactionModel, String> {
     Flux<TransactionModel> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
 }
